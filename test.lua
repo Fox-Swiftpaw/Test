3a406079515541c2c5ac6379369a738964ca96df
Fox Swiftpaw <61133638+Fox-Swiftpaw@users.noreply.github.com>
2025-05-16 00:58:57 -0500

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,12 @@
+print("hello world")
+x = 11
+y = 37
+print("x is " .. x)
+print("y is " .. y)
+if x > y then
+    print("x is greater!")
+elseif x < y then
+    print("y is greater!")
+else
+    print("x and y are equal!")
+end
